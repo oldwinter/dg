844a5c1ed56340c4697e413b2a468a172890307d
cdd <cdd2zju@gmail.com>
2023-03-14 00:45:59 +0800

    qq

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -22,16 +22,16 @@ class BidirectionalLinksGenerator < Jekyll::Generator
           )
         ).gsub('\_', '[ _]').gsub('\-', '[ -]').capitalize
 
-        title_from_data = note_potentially_linked_to.data['title']
-        if title_from_data
-          title_from_data = Regexp.escape(title_from_data)
-        end
+        # title_from_data = note_potentially_linked_to.data['title']
+        # if title_from_data
+        #   title_from_data = Regexp.escape(title_from_data)
+        # end
 
         new_href = "#{site.baseurl}#{note_potentially_linked_to.url}#{link_extension}"
         anchor_tag = "<a class='internal-link' href='#{new_href}'>\\1</a>"
 
         # Replace double-bracketed links with label using note title
-        # [[A note about cats|this is a link to the note about cats]]
+        [[A note about cats|this is a link to the note about cats]]
         current_note.content.gsub!(
           /\[\[#{note_title_regexp_pattern}\|(.+?)(?=\])\]\]/i,
           anchor_tag
@@ -39,17 +39,17 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
         # Replace double-bracketed links with label using note filename
         # [[cats|this is a link to the note about cats]]
-        current_note.content.gsub!(
-          /\[\[#{title_from_data}\|(.+?)(?=\])\]\]/i,
-          anchor_tag
-        )
+        # current_note.content.gsub!(
+        #   /\[\[#{title_from_data}\|(.+?)(?=\])\]\]/i,
+        #   anchor_tag
+        # )
 
         # Replace double-bracketed links using note title
         # [[a note about cats]]
-        current_note.content.gsub!(
-          /\[\[(#{title_from_data})\]\]/i,
-          anchor_tag
-        )
+        # current_note.content.gsub!(
+        #   /\[\[(#{title_from_data})\]\]/i,
+        #   anchor_tag
+        # )
 
         # Replace double-bracketed links using note filename
         # [[cats]]
